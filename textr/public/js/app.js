9a55aadd61f5ffd6e880c2ad4af5cf80e7f544b6
Ben Ellentuck <ellentuckben@gmail.com>
2017-03-10 13:03:28 -0500

    Fleshing out textr functionality & stylez

--- textr/public/js/app.js
+++ textr/public/js/app.js
@@ -1,10 +1,48 @@
-// Handling form submission
+(function() {
+  'use strict';
 
+// Handling form submission
 // Get DOM object for each form input element
 var numberField = document.querySelector('input[name=number]');
 var textField = document.querySelector('input[name=text]');
 var button = document.querySelector('input[type=button]');
 var msg = document.querySelector('.response');
+
+// Web Notifications permission request [W3C API]
+var permission = 'denied';
+try {
+  Notification.requestPermission().then(function(status) {
+    permission = status;
+    console.log('Web notification status: ' + permission); // either 'granted' or 'denied'
+  });
+} catch (error) { // Safari 9 requestPermission doesn't return a promise
+  Notification.requestPermission(function(status) {
+    permission = status;
+    console.log('Web notification status: ' + permission);
+  });
+}
+
+// Socket.io - receive smsStatus event from server
+var socket = io();
+socket.on('connect', function() {
+  console.log('Socket connected');
+});
+socket.on('smsStatus', function(data) {
+  console.log(data);
+  if (!data) return;
+  if (data.error) {
+    displayStatus('Error: ' + data.error, permission);
+  } else {
+    displayStatus('Message ID ' + data.id +
+                  ' successfully sent to ' + data.number);
+  }
+});
+
+// Repopulate number field with most recent number
+var lastNumber = localStorage.getItem('number');
+if (lastNumber) {
+  numberField.value = lastNumber;
+}
 
 // UI events
 // Submit form via Return/Enter key...
@@ -18,15 +56,45 @@ button.addEventListener('click', send, false);
 function send() {
   // 1) Get values from inputs
   var number = numberField.value.replace(/\D/g, ''); // Remove non-numeric chars
-  var text = textField.value;
+  if (!number) return;
+  var text = textField.value || 'Hey girl hey';
+  localStorage.setItem('number', number);
+
   // 2) Post values to server via Fetch API
+  if (!self.fetch) {
+    displayStatus("Sorry, your browser doesn't support the required Fetch API.");
+    return;
+    // XHR could work as fallback option
+  }
   fetch('/', {
     method: 'post',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stingify({number: number, text: text})
-  })
-  .then(function(res){ console.log(res) })
-  .catch(function(error){ console.log(error)});
+    body: JSON.stringify({
+      number: number,
+      text: text
+    })
+  }).then(function(res){ console.log(res) })
+  .catch(function(error){ console.log(error) });
 }
+
+function displayStatus(message, notification) {
+  console.log(notification);
+
+  if (notification === 'granted') {
+    var notification = new Notification('Nexmo', {
+      body: message,
+      icon: 'images/icon-nexmo.png'
+    });
+  } else { // if notification denied by user
+    msg.classList.add('poof');
+    msg.textContent = message;
+    msg.addEventListener('animationend', function() {
+      msg.textContent = '';
+      msg.classList.remove('poof');
+    }, false);
+  }
+}
+
+})();
--- textr/server/index.js
+++ textr/server/index.js
@@ -1,26 +1,46 @@
 'use strict';
 
 /** Questions:
- 1) To what extent are require statements deprecated in favor of import statements?
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  const ejs = require('ejs');
+ 1) To what extent are require statements deprecated in favor of import statements? Why couldn't import statements work here?
  2) To what extent is ejs deprecated in favor of done.js?
 */
 
 /** Glossary:
- - body-parser: middleware to parse form input onto a body property of the JSONified request.
+ - body-parser: middleware to parse form input onto a body property of the JSONified (stringified) request.
  - ejs: HTML templating tool.
+ - Socket.io: enabling server API <-> browser/client API "real-time bidirectional event-based communication."
 */
 
 // Setup
-import express from 'express';
-import bodyParser from 'body-parser';
-import ejs from 'ejs';
+const config = require('./config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
 
 const app = express();
-const server = app.listen(4000);
+const server = app.listen(4000, () => {
+  console.log('Express server listening on port %d in %s mode',
+              server.address().port, app.settings.env);
+});
 
+// Nexmo configs
+const nexmo = new Nexmo({
+  apiKey: config.API_KEY,
+  apiSecret: config.API_SECRET,
+}, {debug: true});
+
+// Socket.io configs
+const io = socketio(server);
+io.on('connection', (socket) => {
+  console.log('Socket connected');
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected');
+  });
+});
+
+// Express configs
 app.set('views', __dirname + '/../views');
 app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
@@ -28,7 +48,44 @@ app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serving HTML
+// Express routes //
+// 1) GET request: Serving HTML
 app.get('/', (req, res) => {
   res.render('index');
+});
+// 2) POST request: Take form input values from request
+app.post('/', (req, res) => {
+  res.send(req.body);
+  // console.log(req.body);
+  let toNumber = req.body.number;
+  let text = req.body.text;
+  let data = {};
+
+  // Sending SMS via Nexmo
+  nexmo.message.sendSms(
+    config.NUMBER, toNumber, text, {type: 'unicode'},
+    (err, responseData) => {
+      if (err) {
+        data = {error: err};
+      } else {
+        //console.dir(responseData);
+        if (responseData.messages[0]['error-text']) {
+          data = {error: responseData.messages[0]['error-text']};
+        } else {
+        // Socket.io
+        let data = {id: responseData.messages[0]['message-id'],
+                    number: responseData.messages[0]['to']};
+        io.emit('smsStatus', data);
+      }
+    }
+  );
+
+  // // Nexmo basic number insight - info about the phone number
+  // nexmo.numberInsight.get({level: 'basic', number: toNumber},
+  //                         (err, responseData) => {
+  //                           if (err) console.log(err);
+  //                           else {
+  //                             console.dir(responseData);
+  //                           }
+  //                         });
 });
